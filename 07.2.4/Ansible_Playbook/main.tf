943a6b27d9d76e11a97d44db33ad8df9b6166959
Timo Grautstueck <timo.grautstueck@web.de>
2024-12-21 08:55:34 +0100

    Falsche Dateiendung vom Playbook

--- 07.2.4/Ansible_Playbook/main.tf
+++ 07.2.4/Ansible_Playbook/main.tf
@@ -3,7 +3,7 @@ resource "null_resource" "ansible-per-local-exec" {
   provisioner "local-exec" {
     command = <<EOL
       export ANSIBLE_HOST_KEY_CHECKING=False;
-      ansible-playbook -u ${var.user} --private-key ${var.ssh_private_key} -i self.public_ip web-playbook.yaml
+      ansible-playbook -u ${var.user} --private-key ${var.ssh_private_key} -i self.public_ip web-playbook.yml
     EOL
   }
 }
